e81e1b7b47f0983b27841e9dc0e88d1274208bcc
Şeyma Gündoğdu <symagn2@gmail.com>
2022-10-20 17:23:25 +0300

    rug

--- src/rug/components/Palette.js
+++ src/rug/components/Palette.js
@@ -1,11 +1,12 @@
 import {useState} from 'react'
 import {send} from "../socketApi"
 function Palette() {
-     const [first, setfirst] = useState([])
+     const [color, setColor] = useState("")
   return (
     <div className='palette'>Palette
-    <input  type="color"/>
-    <button>Click</button>
+    <input  type="color" value={color} onChange={(e) => setColor(e.target.value)}/>
+    <button onClick={() => send(color) }>Click</button>
+ 
     </div>
   )
 }
